9f5541b722be9c57d8a6c32807256f992d63e3ba
notepraiya <notepraiya@gmail.com>
2020-12-29 18:19:02 +0700

    add sliver grid

---
+++ lib/landing.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBar(
+            pinned: true,
+            expandedHeight: 250.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                '-',
+              ),
+            ),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 1.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('Grid Item $index'),
+                );
+              },
+              childCount: 30,
+            ),
+          ),
+
+          // SliverFixedExtentList(
+          //   itemExtent: 50.0,
+          //   delegate: SliverChildBuilderDelegate(
+          //         (BuildContext context, int index) {
+          //       return Container(
+          //         alignment: Alignment.center,
+          //         color: Colors.lightBlue[100 * (index % 9)],
+          //         child: Text('List Item $index'),
+          //       );
+          //     },
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:notepraiya_github_io/hello_word.dart';
+//import 'package:notepraiya_github_io/hello_word.dart';
+import 'landing.dart';
 
 const app_title = "NotePraiya's Portfolio";
 
 void main() {
-  runApp(MyApp());
+  runApp(MatApp());
 }
 
-class MyApp extends StatelessWidget {
+class MatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,36 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: app_title),
+      home: Landing(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: HelloWorld(),
-    );
-  }
-}
